4fd0ff08650fe705222a6a772355aec7251dae69
Lydia Sims <sims.lydia88@gmail.com>
2024-02-01 19:53:45 -0600

    Complete the checkoutBook function

--- src/library.js
+++ src/library.js
@@ -1,8 +1,44 @@
+const { should } = require("chai")
 
+function createLibrary(name){
+  var library = {
+    name:name,
+  }
+  library.shelves = {fantasy:[], fiction:[], nonFiction:[]}
+  
+  return library
+}
+
+function addBook(library, book){
+   if(book.genre ==="fantasy"){
+    library.shelves.fantasy.push(book)
+  } else if(book.genre === "nonFiction"){
+    library.shelves.nonFiction.push(book)
+  } else{
+    library.shelves.fiction.push(book)
+  }
+  return library
+}
+
+
+function checkoutBook(library, title, genre) {
+  for(var i = 0; i < library.shelves[genre].length; i++){
+    if(title === library.shelves[genre][i].title){
+       library.shelves[genre].splice(i,1);
+       return `You have now checked out ${title} from the ${library.name}.`
+      }
+    }
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}.`
+}
+
+
+function takeStock(){
+
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
