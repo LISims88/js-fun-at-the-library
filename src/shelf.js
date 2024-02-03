a275765017d1dd8629622c50f413ade5b975a319
Lydia Sims <sims.lydia88@gmail.com>
2024-02-03 16:29:38 -0600

    Complete shelfBook of shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,25 @@
+function shelfBook(book, sciFiShelf){
+  if(book.genre==="sciFi" && sciFiShelf.length <= 2){
+    sciFiShelf.unshift(book)
+  }
+    return sciFiShelf 
+}
+
+function unshelfBook(){
+
+}
+
+function listTitles(){
+
+}
+
+function searchShelf(){
+
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
--- test/shelf-test.js
+++ test/shelf-test.js
@@ -9,11 +9,11 @@ var {
 
 describe("shelf.js", function() {
   describe("shelfBook", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(shelfBook);
     });
 
-    it.skip("should add books to a specific shelf", function() {
+    it("should add books to a specific shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -27,6 +27,7 @@ describe("shelf.js", function() {
         genre: "sciFi"
       };
       var sciFiShelf = [];
+      
 
       shelfBook(hyperion, sciFiShelf);
       shelfBook(dune, sciFiShelf);
@@ -35,7 +36,7 @@ describe("shelf.js", function() {
       assert.equal(sciFiShelf[1], hyperion);
     });
 
-    it.skip("should add another book to the shelf", function() {
+    it("should add another book to the shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -64,7 +65,7 @@ describe("shelf.js", function() {
       assert.equal(sciFiShelf[2], hyperion);
     });
 
-    it.skip("shelves can only hold a certain amount of books", function () {
+    it("shelves can only hold a certain amount of books", function () {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -104,7 +105,7 @@ describe("shelf.js", function() {
   });
 
   describe("unshelfBook", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(unshelfBook);
     });
 
